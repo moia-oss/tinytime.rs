632f7a8446db054b6f7292936b0083ba3afbe094
Rinde van Lon <rindevanlon@gmail.com>
2025-02-02 11:16:53 +0000

    chore: update rand to 0.9.0 (#101)

--- src/rand.rs
+++ src/rand.rs
@@ -1,17 +1,17 @@
-use rand::distributions::uniform::SampleBorrow;
-use rand::distributions::uniform::SampleUniform;
-use rand::distributions::uniform::UniformInt;
-use rand::distributions::uniform::UniformSampler;
-use rand::distributions::Distribution;
-use rand::distributions::Standard;
+use rand::distr::uniform::SampleBorrow;
+use rand::distr::uniform::SampleUniform;
+use rand::distr::uniform::UniformInt;
+use rand::distr::uniform::UniformSampler;
+use rand::distr::Distribution;
+use rand::distr::StandardUniform;
 use rand::Rng;
 
 use crate::Duration;
 use crate::Time;
 
-impl Distribution<Time> for Standard {
+impl Distribution<Time> for StandardUniform {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Time {
-        Time(rng.gen())
+        Time(rng.random())
     }
 }
 
@@ -21,20 +21,20 @@ pub struct UniformTime(UniformInt<i64>);
 impl UniformSampler for UniformTime {
     type X = Time;
 
-    fn new<B1, B2>(low: B1, high: B2) -> Self
+    fn new<B1, B2>(low: B1, high: B2) -> Result<Self, rand::distr::uniform::Error>
     where
         B1: SampleBorrow<Self::X> + Sized,
         B2: SampleBorrow<Self::X> + Sized,
     {
-        Self(UniformInt::new(low.borrow().0, high.borrow().0))
+        UniformInt::new(low.borrow().0, high.borrow().0).map(Self)
     }
 
-    fn new_inclusive<B1, B2>(low: B1, high: B2) -> Self
+    fn new_inclusive<B1, B2>(low: B1, high: B2) -> Result<Self, rand::distr::uniform::Error>
     where
         B1: SampleBorrow<Self::X> + Sized,
         B2: SampleBorrow<Self::X> + Sized,
     {
-        Self(UniformInt::new_inclusive(low.borrow().0, high.borrow().0))
+        UniformInt::new_inclusive(low.borrow().0, high.borrow().0).map(Self)
     }
 
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Self::X {
@@ -46,9 +46,9 @@ impl SampleUniform for Time {
     type Sampler = UniformTime;
 }
 
-impl Distribution<Duration> for Standard {
+impl Distribution<Duration> for StandardUniform {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Duration {
-        Duration(rng.gen())
+        Duration(rng.random())
     }
 }
 
@@ -58,20 +58,20 @@ pub struct UniformDuration(UniformInt<i64>);
 impl UniformSampler for UniformDuration {
     type X = Duration;
 
-    fn new<B1, B2>(low: B1, high: B2) -> Self
+    fn new<B1, B2>(low: B1, high: B2) -> Result<Self, rand::distr::uniform::Error>
     where
         B1: SampleBorrow<Self::X> + Sized,
         B2: SampleBorrow<Self::X> + Sized,
     {
-        Self(UniformInt::new(low.borrow().0, high.borrow().0))
+        UniformInt::new(low.borrow().0, high.borrow().0).map(Self)
     }
 
-    fn new_inclusive<B1, B2>(low: B1, high: B2) -> Self
+    fn new_inclusive<B1, B2>(low: B1, high: B2) -> Result<Self, rand::distr::uniform::Error>
     where
         B1: SampleBorrow<Self::X> + Sized,
         B2: SampleBorrow<Self::X> + Sized,
     {
-        Self(UniformInt::new_inclusive(low.borrow().0, high.borrow().0))
+        UniformInt::new_inclusive(low.borrow().0, high.borrow().0).map(Self)
     }
 
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Self::X {
@@ -90,19 +90,19 @@ mod tests {
     #[test]
     fn standard_time() {
         let _: Time = rand::random();
-        let _: Time = rand::thread_rng().gen();
+        let _: Time = rand::rng().random();
     }
 
     #[test]
     fn uniform_time() {
-        let mut rng = rand::thread_rng();
-        assert_eq!(Time::EPOCH, rng.gen_range(Time::EPOCH..Time::millis(1)));
-        assert_eq!(Time::EPOCH, rng.gen_range(Time::EPOCH..=Time::EPOCH));
+        let mut rng = rand::rng();
+        assert_eq!(Time::EPOCH, rng.random_range(Time::EPOCH..Time::millis(1)));
+        assert_eq!(Time::EPOCH, rng.random_range(Time::EPOCH..=Time::EPOCH));
 
         let low = Time::millis(100);
         let high = Time::millis(110);
         for _ in 0..1000 {
-            let x = rng.gen_range(low..high);
+            let x = rng.random_range(low..high);
             assert!((low..high).contains(&x));
         }
     }
@@ -110,25 +110,25 @@ mod tests {
     #[test]
     fn standard_duration() {
         let _: Duration = rand::random();
-        let _: Duration = rand::thread_rng().gen();
+        let _: Duration = rand::rng().random();
     }
 
     #[test]
     fn uniform_duration() {
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::rng();
         assert_eq!(
             Duration::ZERO,
-            rng.gen_range(Duration::ZERO..Duration::millis(1))
+            rng.random_range(Duration::ZERO..Duration::millis(1))
         );
         assert_eq!(
             Duration::ZERO,
-            rng.gen_range(Duration::ZERO..=Duration::ZERO)
+            rng.random_range(Duration::ZERO..=Duration::ZERO)
         );
 
         let low = Duration::millis(100);
         let high = Duration::millis(110);
         for _ in 0..1000 {
-            let x = rng.gen_range(low..high);
+            let x = rng.random_range(low..high);
             assert!((low..high).contains(&x));
         }
     }
